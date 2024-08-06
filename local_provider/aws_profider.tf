adf580abeae53baa326f1b73a177bf6f9dcde676
hassannader2040 <79514700+hasannader2040@users.noreply.github.com>
2024-08-06 11:15:27 +0300

    that one

--- local_provider/aws_profider.tf
+++ local_provider/aws_profider.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e32ca89a9ad7022db12edce2b2218d8dbc40c64dc699d25722b94a384e795985
-size 100
+oid sha256:71b57098f14ef46acb0cdcd5c62c8ecc36f5c8311e2d166c52acbc7dc146fe98
+size 101
